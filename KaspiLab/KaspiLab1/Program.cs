9edf33bc921a2e89a455f01b278a4c478a176fbe
letsleroyme <letsleroyme@gmail.com>
2021-03-07 12:25:36 +0700

    Init
    Создание репы и первая лаба.

---
+++ KaspiLab/KaspiLab1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KaspiLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[10];
+            int summ = 0;
+            Random rand = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = rand.Next(0, 100);
+                Console.Write($@"{mas[i]} ");
+                summ += mas[i];
+            }
+            Console.WriteLine();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (IsEvenNumber(mas[i]))
+                    Console.WriteLine($@"Четное число: {mas[i]}");
+
+                if (IsSimpleNumber(mas[i]))
+                    Console.WriteLine($@"Простое число: {mas[i]}");
+            }
+            Console.WriteLine($@"Сумма всех чисел: {summ}");
+
+
+
+            Console.ReadKey();
+        }
+
+        private static bool IsEvenNumber(int n)
+        {
+            return n % 2 == 0;
+        }
+
+        private static bool IsSimpleNumber(int n)
+        {
+            if (n > 1)
+            {
+                for (int i = 2; i < n; i++)
+                    if (n % i == 0) return false;
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+  
+}
