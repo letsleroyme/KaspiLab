82b512d66be48382ad88e898418f701d20687ea9
letsleroyme <letsleroyme@gmail.com>
2021-03-09 13:11:19 +0700

    Задание 2
    Задание 2 с лифтом.
    
    Создать класс, описывающий лифт.
    Методы (как минимум)
    - поехать на заданный этаж
    - вызвать лифт на заданный этаж
    Загрузить/выгрузить пассажиров
    
    Свойства (как минимум):
    - этажность здания
    - грузоподъемность (людей)
    
    Лифт не должен:
    - попасть на несуществующий этаж
    - открывать двери между этажами
    - ехать с открытыми дверями
    - ехать с перегрузкой
    - запускать людей в момент движения
    
    В каждом действии выводить на консоль сообщение, что выполняется
    При передвижении выводить сообщение для каждого этажа

---
+++ KaspiLab/KaspiLab2/Elevator.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaspiLab2
+{
+    class Elevator
+    {
+
+        public Elevator(int countoffloors, int capacity)
+        {
+            CountOfFloors = countoffloors;
+            Capacity = capacity;
+        }
+
+
+
+        /// <summary>
+        /// Этажность здания
+        /// </summary>
+        private int _countOfFloors;
+        public int CountOfFloors
+        {
+            private set
+            {
+                if (value <= 0)
+                    throw new Exception("В здании не может быть 0 или менее этажей!");
+                else
+                    _countOfFloors = value;
+            }
+            get
+            {
+                return _countOfFloors;
+            }
+        }
+
+        /// <summary>
+        /// Грузоподъемность
+        /// </summary>
+        private int _capacity;
+        public int Capacity
+        {
+            private set
+            {
+                if (value <= 0)
+                    throw new Exception("Грузоподъемность не может быть 0 или менее людей!");
+                else
+                    _capacity = value;
+            }
+            get
+            {
+                return _capacity;
+            }
+        }
+
+        /// <summary>
+        /// Текущий этаж, на котором находится лифт
+        /// </summary>
+        private int _currentFloor = 0;
+
+        /// <summary>
+        /// Переменная, необходимая для блокирования действий во время движения
+        /// </summary>
+        private bool _doorClosed = true;
+
+        public void LiftMovement(int DestinationFloor, int PeopleCount)
+        {
+            if (CheckBeforeMovement(DestinationFloor, PeopleCount))
+            {
+                Console.WriteLine($"Открываю дверь");
+                Console.WriteLine($"Начинаю движение");
+                    
+                if (DestinationFloor > _currentFloor)
+                {
+                    for (int i = _currentFloor; i <= DestinationFloor; i++)
+                    {
+                        _currentFloor = i;
+                        Console.WriteLine($"Текущий этаж: {i}");
+                    }
+                }
+                else
+                {
+                    for (int i = _currentFloor; i >= DestinationFloor; i--)
+                    {
+                        _currentFloor = i;
+                        Console.WriteLine($"Текущий этаж: {i}");
+                    }
+                }
+                Console.WriteLine($"Закончил движение");
+            }
+        }
+
+
+        public void CallElevator(int DestinationFloor)
+        {
+
+            if (CheckBeforeMovement(DestinationFloor))
+            {
+                Console.WriteLine($"Вызван лифт на этаж {DestinationFloor}");
+                if (DestinationFloor > _currentFloor)
+                {
+                    for (int i = _currentFloor; i <= DestinationFloor; i++)
+                    {
+                        _currentFloor = i;
+                        Console.WriteLine($"Текущий этаж: {i}");
+                    }
+                }
+                else
+                {
+                    for (int i = _currentFloor; i >= DestinationFloor; i--)
+                    {
+                        _currentFloor = i;
+                        Console.WriteLine($"Текущий этаж: {i}");
+                    }
+                }
+                Console.WriteLine($"Закончил движение");
+            }
+        }
+
+        private void WorkWithDoors()
+        {
+            if (_doorClosed)
+            {
+                Console.WriteLine("Открываю дверь");
+                _doorClosed = false;
+            }
+            else
+            {
+                Console.WriteLine("Открываю дверь");
+                _doorClosed = true;
+            }
+        }
+
+
+
+
+        private bool CheckBeforeMovement(int DestinationFloor, int PeopleCount = 0)
+        {
+            if (_currentFloor == DestinationFloor)
+            {
+                Console.WriteLine("Лифт находится на вызванном этаже!");
+                return false;
+            }
+            if (PeopleCount > _capacity)
+            {
+                Console.WriteLine("Перегрузка!");
+                return false;
+            }
+            if (DestinationFloor > _countOfFloors || DestinationFloor < _countOfFloors)
+            {
+                Console.WriteLine("Лифт вызван на несуществующий эатж!");
+                return false;
+            }
+
+
+            return true;
+        }
+
+
+
+
+    }
+}
---
+++ KaspiLab/KaspiLab2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaspiLab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Elevator el = new Elevator(10, 10);
+                el.CallElevator(9);
+                el.LiftMovement(1, 9);
+                Elevator el2 = new Elevator(-1, 12);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
