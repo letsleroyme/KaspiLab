3b6cc0dedf792ae1143f8e74ba6ee048550533e0
letsleroyme <letsleroyme@gmail.com>
2021-03-11 15:08:14 +0700

    Base interfaces

---
+++ KaspiLab/Lab3/Interfaces/IWarehouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Interfaces
+{
+    interface IWarehouse
+    {
+        void AddProduct(Product product);
+        Product MoveProduct(Product product);
+        Product SearchProduct(string sku);
+        double Calculate();
+        void SetEmployee();
+
+    }
+}
--- KaspiLab/Lab3/Warehouse/OpenWarehouse.cs
+++ KaspiLab/Lab3/Warehouse/OpenWarehouse.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Lab3.Products;
+using Lab3.Interfaces;
 
 namespace Lab3.Warehouse
 {
-    class OpenWarehouse : Warehouse 
+    class OpenWarehouse : Warehouse, IWarehouse
     {
-        public Product[] Products 
+        private List<Product> _products;
+        /*public Product[] Products 
         { 
             set
             {
@@ -17,13 +19,49 @@ namespace Lab3.Warehouse
                 {
                     if (product is BulkProduct bulkProduct)
                         throw new Exception("На открытом складе не могут храниться сыпучие продукты");
+                    _products = Products;
                 }
             }
 
             get
             {
-                return Products;
+                return _products;
             }
+        }*/
+
+
+        public void AddProduct(Product product)
+        {
+            if (!(product is BulkProduct))
+                _products.Add(product);
         }
+
+        public double Calculate()
+        {
+            double totalcost = 0;
+            foreach (var product in _products)
+                totalcost += product.Cost;
+            
+            return totalcost;
+        }
+
+        public Product MoveProduct(Product product)
+        {
+            _products.Remove(product);
+            return product;
+        }
+
+        public Product SearchProduct(string sku)
+        {
+            return _products.Where(x => x.SKU == sku).FirstOrDefault();
+        }
+
+        public void SetEmployee(Employee emp)
+        {
+            base.Manager = emp;
+        }
+
+
+
     }
 }
