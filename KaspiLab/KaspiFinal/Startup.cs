06c40ad69ab5171a148040948afa4340e1374e51
letsleroyme <letsleroyme@gmail.com>
2021-04-29 15:26:51 +0700

    KaspiFinal innit

---
+++ KaspiLab/KaspiFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KaspiFinal.Startup))]
+namespace KaspiFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
